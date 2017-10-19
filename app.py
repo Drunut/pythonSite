82f4099b4eb008b60ba66838151671503ffb1345
Drunut <tekulve.nikolaas@gmail.com>
2017-10-19 02:06:52 -0600

    Put in some TODO's in app.py Added debugging / Live Refresh to the .bat

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+# TODO: Figure out with I want to use Flask or Django
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+# TODO: Figure out how to deliver a specific web page
+
+if __name__ == '__main__':
+    app.run()
