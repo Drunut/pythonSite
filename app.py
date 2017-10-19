5de9447469af11be2fa530d52e1176e5bb8120a4
Drunut <drunut@gmail.com>
2017-10-19 16:42:37 -0600

    Restructured project, created templates

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
+#!/usr/bin/env python
+
 # TODO: Figure out with I want to use Flask or Django
-from flask import Flask
-app = Flask(__name__)
+# So we can use render_template function, then so we can use Flask constructor
+import flask
+
+app = flask.Flask(__name__)
 
 @app.route('/')
+def homePage():
+    return flask.render_template('index.html')
+
+@app.route('/hello/<name>/')
+def hello(name):
+    # Displays the page greeting who ever comes to visit it.
+    return flask.render_template('hello.html', name=name)
+
+@app.route('/helloWorld')
 def hello_world():
     return 'Hello, World!'
 # TODO: Figure out how to deliver a specific web page
 
 if __name__ == '__main__':
-    app.run()
+    app.run(use_reloader=True, debug=True)
